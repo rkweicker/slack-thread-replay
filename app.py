e56f685ff4de95c48f62605ce2b3f092b3e29b60
Ryan Weicker <rkweicker@gmail.com>
2022-06-01 20:44:42 -0400

    time to use shortcuts!

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import time
 import datetime
 import json
@@ -14,8 +15,9 @@ from slack_sdk.errors import SlackApiError
 with open('tokens.json') as f:
    data = json.load(f)
 
-SLACK_APP_TOKEN = data['SLACK_APP_TOKEN']
-SLACK_BOT_TOKEN = data['SLACK_BOT_TOKEN']
+
+SLACK_APP_TOKEN = data['prod']['SLACK_APP_TOKEN']
+SLACK_BOT_TOKEN = data['prod']['SLACK_BOT_TOKEN']
 
 
 # Install the Slack app and get xoxb- token in advance
@@ -108,16 +110,29 @@ def hello_command(ack, body):
 
 # Given the details of the thread where the replay is requested, get all the replies
 # and send as an IM to the requesting user
-def send_ims(channel_id,thread_ts,end_ts, user, send_message=True):
+def send_ims(channel_id,thread_ts,start_ts,end_ts, user):
 
     #TODO - try/catch
     # Gets all replies
-    replies = client.conversations_replies(channel=channel_id, ts=thread_ts)
+    try: 
+        replies = client.conversations_replies(channel=channel_id, ts=thread_ts)
+    except SlackApiError as e:
+        print(e.response["error"])
+        try:
+            result = client.conversations_join(channel=channel_id)
+            print(result)
+            replies = client.conversations_replies(channel=channel_id, ts=thread_ts)
+        except SlackApiError as e:
+            print(e.response)
+            return
 
-    # Delay 15 seconds because of weird issue where scheduling happens in the past
+
+
+    # Delay 10 seconds because of weird issue where scheduling happens in the past
     # TODO - check this
-    start_time = time.time() + 15
-    print("Current time: " + str(print_date(time.time())))
+    start_time = time.time() + 10
+    print('start time: ' + str(int(start_time)))
+    
     
     # First message of the thread should be a new post via chat_postMessage so it's thread_ts can be used
     # for subsequent replies
@@ -125,44 +140,48 @@ def send_ims(channel_id,thread_ts,end_ts, user, send_message=True):
     
     for message in replies['messages']:
         ts = message['ts']
-        
-        # Don't need to send the bot's reply message
-        if ts == end_ts:
-            break
 
-        # Calculate the delta between the current message and the original message
-        delta = float(ts) - float(thread_ts)
-        bot_message = "<@" + message['user'] + ">:  " + message['text']    
+        # if the user shares a message IN the thread, skip all the messages until then:
+        if ts < start_ts:
+            continue
         
-        schedule_timestamp = start_time + delta
+        if first_message:
+            #TODO - try/catch
 
-        #TODO probably don't need the send_message test mode anymore
-        if send_message:
+            bot_message = "<@" + message['user'] + ">:  " + message['text']
             print(bot_message)
-            
-            if first_message:
-                #TODO - try/catch
-                result = client.chat_postMessage(
-                    channel=user,
-                    text=bot_message,
-                    blocks=format_blocks(message['blocks']) if 'blocks' in message.keys() else None,
-                    post_at=int(schedule_timestamp),
-                    link_names=True
-                )
-                # Get the resulting ts to use as the thread_ts for all scheduled messages
-                first_ts = result['ts']
-                first_message = False
-            else :
-                
-                # Limit - > 30 / 5 minutes will fail per documentations
-                #TODO - try/catch
-                result = client.chat_scheduleMessage(
-                    channel=user,
-                    text=bot_message,
-                    blocks=format_blocks(message['blocks']) if 'blocks' in message.keys() else None,
-                    post_at=int(schedule_timestamp),
-                    thread_ts=first_ts,
-                    link_names=True)
+
+            result = client.chat_postMessage(
+                channel=user,
+                text=bot_message,
+                blocks=format_blocks(message['blocks']) if 'blocks' in message.keys() else None,
+                link_names=False
+            )
+            # Get the resulting ts to use as the thread_ts for all scheduled messages
+            first_ts = result['ts']
+            first_message = False
+        else :
+
+            # Don't need to send the bot's reply message
+            if ts == end_ts:
+                break
+
+            # Calculate the delta between the current message and the original message
+            delta = float(ts) - float(thread_ts)
+            bot_message = "<@" + message['user'] + ">:  " + message['text']  
+            print(bot_message)
+            schedule_timestamp = float(start_time) + float(delta)
+            print("Scheduled Time: " + str(int(schedule_timestamp)) + " Current time: " + str(int(time.time())))
+            # Limit - > 30 / 5 minutes will fail per documentations
+            #TODO - try/catch
+            result = client.chat_scheduleMessage(
+                channel=user,
+                text=bot_message,
+                blocks=format_blocks(message['blocks']) if 'blocks' in message.keys() else None,
+                post_at=int(schedule_timestamp),
+                thread_ts=first_ts,
+                link_names=False)
+            #print("Scheduled Time: " + str(int(schedule_timestamp)) + " Current time: " + str(int(time.time())))
 
 
 
@@ -179,11 +198,38 @@ def log_message(event):
 
     return user + " called me and said: " + event['text']
 
+def is_message_shared(event):
+
+    if check_for_single_attachments(event):
+        return event['attachments'][0]['is_share']
+
+    return False
+
+
+def check_for_single_attachments(event):
+
+    if 'attachments' in event.keys():
+        if len(event['attachments']) == 1:
+            return True
+    return False
+
+
 # Handles any IMs to the bot
 @app.event("message")
 def im_message(event,say):
 
-    print(log_message(event))
+#    print(log_message(event))
+    print(event)
+
+    #if not check_for_single_attachments(event):
+    #    say('Only share one thread please!')
+    #    return
+
+    #if is_message_shared(event) and event['text'].lower() == KEY_WORD:
+    #    channel_id = event['attachments'][0]['channel_id']
+    #    thread_ts = event['attachments'][0]['ts']
+    #    send_ims(channel_id,thread_ts,thread_ts,time.time(),event['user'])
+    #    return
 
     if event['text'].lower() == KEY_WORD_DELETE:
         say("Stopping all scheduled messages.")
@@ -191,13 +237,90 @@ def im_message(event,say):
     else:
         say('I can do two things: \n'+
             'To stop a current thread replay type `!replay-delete`. Note - slack has a bug where anything scheduled in the next 5 minutes can not be deleted\n' +
-            'To start a new thread replay, go to that thread and mention me and say `!replay` like this: `@Thread Replay !replay`')
+            'To start a new thread replay, go to any thread, click on the "three dots" action menu, and click Replay Thread! \n' +
+            'You can either start at the top of the thread, or from any message in the thread and the replay will start there.')
 
+
+
+@app.shortcut("thread_replay")
+def handle_replay(ack,shortcut,client):
+    ack()
+    print(shortcut)
+
+    if 'thread_ts' not in shortcut['message'].keys():
+        client.views_open(
+        trigger_id=shortcut["trigger_id"],
+        # A simple view payload for a modal
+        view={
+            "type": "modal",
+            "title": {"type": "plain_text", "text": "Thread Replay"},
+            "close": {"type": "plain_text", "text": "Close"},
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "You can only replay threads!"
+                    }
+                }
+            ]
+        }
+    )
+
+    elif shortcut['message']['thread_ts'] != shortcut['message']['ts']:
+        
+        print("Starting from within the thread")
+        
+        # sharing a thread from this starting point:
+        client.views_open(
+        trigger_id=shortcut["trigger_id"],
+        # A simple view payload for a modal
+        view={
+            "type": "modal",
+            "title": {"type": "plain_text", "text": "Thread Replay"},
+            "close": {"type": "plain_text", "text": "Close"},
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Sending you this thread from this point on - Check your DMs! \n\n "+
+                                "To stop, reply to the DM with ``` !replay-delete ```"
+                    }
+                }
+            ]
+        }
+    )
+        send_ims(shortcut['channel']['id'],shortcut['message']['thread_ts'],shortcut['message']['ts'],shortcut['action_ts'],shortcut['user']['id'])
+        
+    else:
+        client.views_open(
+        trigger_id=shortcut["trigger_id"],
+        # A simple view payload for a modal
+        view={
+            "type": "modal",
+            "title": {"type": "plain_text", "text": "Thread Replay"},
+            "close": {"type": "plain_text", "text": "Close"},
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Sending you this thread - check your DMs!\n\n" +
+                                "To stop, reply to the DM with ``` !replay-delete ```"
+                    }
+                }
+            ]
+        }
+    )
+        send_ims(shortcut['channel']['id'],shortcut['message']['thread_ts'],shortcut['message']['ts'],shortcut['action_ts'],shortcut['user']['id'])
+    
 
 # Handles all @ mentions
 @app.event("app_mention")
 def event_test(event,say):
     print(log_message(event))
+    print(event)
 
     # For now - if the message is NOT in a thread, do nothing.
     words = event['text'].split()
@@ -218,13 +341,13 @@ def event_test(event,say):
                 text="DMing you this thread!",
                 link_names=True
             )
-            send_ims(event['channel'],event['thread_ts'],event['ts'],event['user'])
+            send_ims(event['channel'],event['thread_ts'],event['thread_ts'],event['ts'],event['user'])
             keyword_spoken = True
             break
 
         elif message.lower() == KEY_WORD_TEST:
             #TODO - try/catch
-            send_ims(event['channel'],event['thread_ts'],event['ts'],event['user'],False)
+            send_ims(event['channel'],event['thread_ts'],event['thread_ts'],event['ts'],event['user'],False)
             result = client.chat_postMessage(
                 channel=event['channel'],
                 thread_ts = event['thread_ts'],
